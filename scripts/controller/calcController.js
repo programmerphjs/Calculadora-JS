26ddd5c1eba617fcf6bc21ab783a7c56ee15fb08
programmerphjs <paulo.h.braguim@gmail.com>
2020-08-19 11:08:44 -0300

    Calculadora Finalizada :)

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
     constructor() {               
         
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = ''; 
         this._operation = [];
@@ -55,12 +57,42 @@ class CalcController {
         this.setLastNumberToDisplay();
 
         this.pastFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+
+            btn.addEventListener('dblclick', e=>{
+
+                this.toggleAudio();
+
+            });
+
+        });
  
+    }
+
+    toggleAudio(){
+
+       this._audioOnOff = !this._audioOnOff;
+
+    }
+
+    playAudio(){
+
+        if(this._audioOnOff){
+
+            this._audio.currentTime = 0;
+
+            this._audio.play();
+
+        }
+
     }
 
     initKeyboard(){
 
         document.addEventListener('keyup', e=>{            
+
+            this.playAudio();
 
             switch (e.key) {
             
@@ -166,8 +198,19 @@ class CalcController {
     }
  
     getResult() {
+        try{
 
-        return eval(this._operation.join(''));
+            return eval(this._operation.join(''));
+
+        }catch(e){
+
+            setTimeout(()=>{
+
+                this.setError();
+
+            }, 1);            
+
+        }        
 
     }
  
@@ -301,6 +344,8 @@ class CalcController {
  
     execBtn(value) {
 
+        this.playAudio();
+
         switch (value) {
             
             case 'ac':
@@ -403,6 +448,14 @@ class CalcController {
     }
  
     set displayCalc(value) {
+
+        if(value.toString().length > 10){
+
+            this.setError();
+            return;
+
+        }
+
         this._displayCalcEl.innerHTML = value;
     }
  
